1a47d29a69b22ae5bf7ffa31d970cc246dcacca9
xLeDocteurx <lenoirc.fr@gmail.com>
2018-10-21 17:43:35 +0200

    finish

--- src/app.es6.js
+++ src/app.es6.js
@@ -1,18 +1,43 @@
-let names = []
-
 let get = id => (document.getElementById(id))
 
 let welcome = () => {
-    name = get('input').value
-    name == '' ? name = 'Père Noël' : ''
+    let name = get('input').value
+    
+    name == '' ? name = 'Père Noël' : name = name[0].toUpperCase() + name.slice(1).toLowerCase()
     alert(`Bonjour ${name} !`)
+        // element[0].toUpperCase()
 
-    names = [...names, name]
-    get('tbody').innerHtml += `<tr>
-        <td>${names.length - 1}</td>
-        <td>${name}</td>
-    </tr>`;
+    handleHistory(name)
 }
 
-// [].map((x)=>(x*2));
-// let names = [];
+const handleHistory = (name) => {
+    if(name){
+        history = [...history, name]
+    }
+
+    let record = get('history')
+    record.innerHTML = ''
+
+    history.forEach((element, index) => {
+        record.innerHTML += `<tr><td>${index}</td> <td>${element}</td></tr>`
+    })
+}
+
+const search = (value) => {
+    let record = get('history')
+    record.innerHTML = ''
+
+    history
+        .filter(element => (
+            element.toLowerCase().slice(0,value.length) == value.toLowerCase())
+        )
+        .forEach((element, index) => {
+            record.innerHTML += `<tr><td>${index}</td> <td>${element}</td></tr>`
+        });
+}
+
+
+//initialisation d'un seed de noms pour tester la fonctionnalité de recherche facilement
+let history = ['Christopher','Christophe','Christelle','Thomas','Martin']
+//affichage au premier chargement de lma page des noms présents dans l'array history
+handleHistory(name)
